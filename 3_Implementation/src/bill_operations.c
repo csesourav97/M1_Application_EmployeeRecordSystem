e4983b7519922f6d9f7c75f2362a4a43a4be1700
csesourav97 <61642281+csesourav97@users.noreply.github.com>
2021-11-25 03:33:11 +0530

    Add files via upload

---
+++ 3_Implementation/src/bill_operations.c
@@ -0,0 +1,26 @@
+#include "../inc/bill_operations.h"
+
+float total_bill(float qty, float price){
+    if( qty <= 0|| price <=0)
+    return 0;
+    float total=0;
+        total = qty * price;
+        return total;        
+    
+}
+
+float net_total(float total){
+    if(total <= 0)
+    return 0;
+    float dis = 0.1 * total;
+    total = total - dis;
+    return total;
+}
+
+float grand_total(float net_total){
+    if( net_total <= 0)
+    return 0;
+    float gst = 0.18 * net_total;
+    float grand_total = net_total + gst;
+    return grand_total;
+}
